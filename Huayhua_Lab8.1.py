e3ebdb1f1dc27e061f542bdff864462bbdde8a65
huayhua58 <98444964+huayhua58@users.noreply.github.com>
2022-01-25 23:39:42 -0600

    Add files via upload

---
+++ Huayhua_Lab8.1.py
@@ -0,0 +1,34 @@
+#Lucero Huayhua
+#Completed
+
+#This programs tells you the number of uppercase,
+#lowercase, digits, and spaces
+#that the text.txt file contains
+
+def main():
+    #open file to read content
+    infile = open('text.txt','r')
+    data = infile.read()
+    #create variables to hold the count of each item
+    upper_count=0
+    lower_count=0
+    digit_count=0
+    space_count=0
+
+#function to count each item
+    for ch in data:
+        if ch.isupper():
+            upper_count += 1
+        if ch.islower():
+            lower_count += 1
+        if ch.isdigit():
+            digit_count += 1
+        if ch.isspace():
+            space_count += 1
+    #display the number of each item
+    print('The number of uppercase letters is  '+ str(upper_count))
+    print('The number of lowercase letters is  '+ str(lower_count))
+    print('The number of uppercase letters is  '+ str(digit_count))
+    print('The number of uppercase letters is  '+ str(space_count))
+
+main()
